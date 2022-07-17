937e9b1f9fee1fdc870609f7f07dc5c52d74432b
ckangwen <2437037469@qq.com>
2022-07-17 22:37:44 +0800

    chore(example): update mobx-miniprogram example

--- packages/example/app.ts
+++ packages/example/app.ts
@@ -1,34 +1,10 @@
 import { usePlugin } from "@charrue/vump";
+import { storeBindingsBehavior } from "mobx-miniprogram-bindings";
 
 usePlugin({
-  behaviors: [
-    Behavior({
-      lifetimes: {
-        attached() {
-          this.setData({
-            dataFromPlugin: "dataFromPlugin",
-          });
-        },
-      },
-    }),
-  ],
+  behaviors: [storeBindingsBehavior],
 });
 
-// app.ts
 App<IAppOption>({
   globalData: {},
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync("logs") || [];
-    logs.unshift(Date.now());
-    wx.setStorageSync("logs", logs);
-
-    // 登录
-    wx.login({
-      success: (res) => {
-        console.log(res.code);
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      },
-    });
-  },
 });
