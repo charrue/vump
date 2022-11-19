a801be9f70627e1336f2f616fe3a34c9c3518883
ckangwen <2437037469@qq.com>
2022-11-19 19:02:38 +0800

    chore: update example

--- packages/example/pages/log/log.ts
+++ packages/example/pages/log/log.ts
@@ -1,47 +1,22 @@
-// pages/log/log.ts
-Page({
-  /**
-   * 页面的初始数据
-   */
-  data: {},
+import { createPage, ref, onShow, onMounted, onShareAppMessage } from "@charrue/vump";
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad() {},
+createPage({
+  setup() {
+    const calls = ref<string[]>([]);
+    onShow(() => {
+      calls.value.push("page show");
+    });
+    onMounted(() => {
+      calls.value.push("page loaded");
+    });
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {},
+    onShareAppMessage(() => {
+      return {
+        title: "自定义标题",
+      };
+    });
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {},
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {},
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {},
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {},
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {},
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {},
+    console.log(calls);
+    return {};
+  },
 });
